bf9917867b494c6a8ebcd9b02ea898573917f860
Shay Moreno <moa129@gmail.com>
2023-07-12 15:03:45 +0300

    improve centering

--- KeepLang/KeyboardViewController.swift
+++ KeepLang/KeyboardViewController.swift
@@ -48,18 +48,48 @@ class KeyboardViewController: UIInputViewController {
     }
 
     func createKeyboardLayout() {
+//        for view in keyboardView.subviews {
+//            view.removeFromSuperview()
+//        }
+//
+//        let keyRows = currentKeyboard.keys
+//
+//        for (rowIndex, rowKeys) in keyRows.enumerated() {
+//            for (index, key) in rowKeys.enumerated() {
+//                let keyButton = UIButton(type: .system)
+//                keyButton.setTitle(String(key), for: [])
+//                keyButton.sizeToFit()
+//                keyButton.frame = CGRect(x: index * 32, y: rowIndex * 50, width: 30, height: 40)
+//                keyButton.addTarget(self, action: #selector(keyPressed(_:)), for: .touchUpInside)
+//
+//                // Modify the appearance of the keys
+//                keyButton.backgroundColor = .white
+//                keyButton.layer.cornerRadius = 5
+//                keyButton.contentEdgeInsets = UIEdgeInsets(top: 2, left: 6, bottom: 2, right: 6)
+//
+//                keyboardView.addSubview(keyButton)
+//            }
+//        }
+
         for view in keyboardView.subviews {
             view.removeFromSuperview()
         }
 
         let keyRows = currentKeyboard.keys
+        let keyWidth: CGFloat = 30
+        let keySpacing: CGFloat = 2
+        let keyHeight: CGFloat = 40
+        let rowSpacing: CGFloat = 50
 
         for (rowIndex, rowKeys) in keyRows.enumerated() {
+            let totalRowWidth = CGFloat(rowKeys.count) * (keyWidth + keySpacing) - keySpacing
+            let startX = (keyboardView.frame.width - totalRowWidth) / 2
+
             for (index, key) in rowKeys.enumerated() {
                 let keyButton = UIButton(type: .system)
                 keyButton.setTitle(String(key), for: [])
                 keyButton.sizeToFit()
-                keyButton.frame = CGRect(x: index * 32, y: rowIndex * 50, width: 30, height: 40)
+                keyButton.frame = CGRect(x: startX + CGFloat(index) * (keyWidth + keySpacing), y: CGFloat(rowIndex) * rowSpacing, width: keyWidth, height: keyHeight)
                 keyButton.addTarget(self, action: #selector(keyPressed(_:)), for: .touchUpInside)
 
                 // Modify the appearance of the keys
@@ -74,9 +104,6 @@ class KeyboardViewController: UIInputViewController {
         // Add a delete button
         let deleteButton = UIButton(type: .system)
         deleteButton.setImage(UIImage(systemName: "delete.left"), for: .normal)
-//        } else {
-//            deleteButton.setTitle("Delete", for: []) // fallback if the image isn't found
-//        }
         deleteButton.sizeToFit()
         deleteButton.frame = CGRect(x: 280, y: 200, width: 40, height: 40)
         deleteButton.addTarget(self, action: #selector(deleteKeyPressed), for: .touchUpInside)
@@ -103,11 +130,6 @@ class KeyboardViewController: UIInputViewController {
         // Add a return button
         let returnButton = UIButton(type: .system)
         returnButton.setImage(UIImage(systemName: "return.right"), for: .normal)
-//        if let returnImage = UIImage(named: "return-icon") { // replace "return-icon" with the name of your image
-//            returnButton.setImage(returnImage, for: .normal)
-//        } else {
-//            returnButton.setTitle("Return", for: []) // fallback if the image isn't found
-//        }
         returnButton.sizeToFit()
         returnButton.frame = CGRect(x: 200, y: 200, width: 70, height: 40)
         returnButton.addTarget(self, action: #selector(returnKeyPressed), for: .touchUpInside)
@@ -159,113 +181,3 @@ class KeyboardViewController: UIInputViewController {
         createKeyboardLayout()
     }
 }
-
-//class KeyboardViewController: UIInputViewController {
-//
-//    var keyboardView: UIView!
-//
-//    var isEnglishKeyboard = true
-//
-//    let englishKeys = "QWERTYUIOPASDFGHJKLZXCVBNM"
-//    let hebrewKeys = "אבגדהוזחטיכלמנסעפצקרשת"
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//
-//        keyboardView = UIView(frame: CGRect(x: 0, y: 0, width: 320, height: 250))
-//        keyboardView.translatesAutoresizingMaskIntoConstraints = false
-//
-//        self.view.addSubview(keyboardView)
-//
-//        createKeyboardLayout()
-//    }
-//
-//    func createKeyboardLayout() {
-//        for view in keyboardView.subviews {
-//            view.removeFromSuperview()
-//        }
-//
-//        let keys = isEnglishKeyboard ? englishKeys : hebrewKeys
-//
-//        for (index, key) in keys.enumerated() {
-//            let keyButton = UIButton(type: .system)
-//            keyButton.setTitle(String(key), for: [])
-//            keyButton.sizeToFit()
-//            keyButton.frame = CGRect(x: (index % 10) * 32, y: (index / 10) * 50, width: 30, height: 40)
-//            keyButton.addTarget(self, action: #selector(keyPressed(_:)), for: .touchUpInside)
-//
-//            keyboardView.addSubview(keyButton)
-//        }
-//
-//        // Add a switch button
-//        let switchButton = UIButton(type: .system)
-//        switchButton.setTitle("Switch", for: [])
-//        switchButton.sizeToFit()
-//        switchButton.frame = CGRect(x: 0, y: 200, width: 80, height: 40)
-//        switchButton.addTarget(self, action: #selector(switchKeyboard), for: .touchUpInside)
-//
-//        keyboardView.addSubview(switchButton)
-//    }
-//
-//    @objc func keyPressed(_ sender: UIButton) {
-//        let key = sender.title(for: []) ?? ""
-//        (textDocumentProxy as UIKeyInput).insertText(key)
-//    }
-//
-//    @objc func switchKeyboard() {
-//        isEnglishKeyboard = !isEnglishKeyboard
-//        createKeyboardLayout()
-//    }
-//}
-
-//class KeyboardViewController: UIInputViewController {
-//
-//    @IBOutlet var nextKeyboardButton: UIButton!
-//
-//    override func updateViewConstraints() {
-//        super.updateViewConstraints()
-//
-//        // Add custom view sizing constraints here
-//    }
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//
-//        // Perform custom UI setup here
-//        self.nextKeyboardButton = UIButton(type: .system)
-//
-//        self.nextKeyboardButton.setTitle(NSLocalizedString("Next Keyboard", comment: "Title for 'Next Keyboard' button"), for: [])
-//        self.nextKeyboardButton.sizeToFit()
-//        self.nextKeyboardButton.translatesAutoresizingMaskIntoConstraints = false
-//
-//        self.nextKeyboardButton.addTarget(self, action: #selector(handleInputModeList(from:with:)), for: .allTouchEvents)
-//
-//        self.view.addSubview(self.nextKeyboardButton)
-//
-//        self.nextKeyboardButton.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
-//        self.nextKeyboardButton.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
-//    }
-//
-//    override func viewWillLayoutSubviews() {
-//        self.nextKeyboardButton.isHidden = !self.needsInputModeSwitchKey
-//        super.viewWillLayoutSubviews()
-//    }
-//
-//    override func textWillChange(_ textInput: UITextInput?) {
-//        // The app is about to change the document's contents. Perform any preparation here.
-//    }
-//
-//    override func textDidChange(_ textInput: UITextInput?) {
-//        // The app has just changed the document's contents, the document context has been updated.
-//
-//        var textColor: UIColor
-//        let proxy = self.textDocumentProxy
-//        if proxy.keyboardAppearance == UIKeyboardAppearance.dark {
-//            textColor = UIColor.white
-//        } else {
-//            textColor = UIColor.black
-//        }
-//        self.nextKeyboardButton.setTitleColor(textColor, for: [])
-//    }
-//
-//}
